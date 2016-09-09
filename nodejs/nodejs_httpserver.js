c397a2cfab907c0607dcda100be75c20b53222ce
reeves55 <lizhongcheng1004@163.com>
2016-09-09 10:36:35 +0800

    测试Vuejs数据绑定结合服务器

--- nodejs/nodejs_httpserver.js
+++ nodejs/nodejs_httpserver.js
@@ -3,9 +3,10 @@
  */
 var http = require('http');
 console.log('test node.js http_server...');
-http.createServer(function (request,response) {
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('Hello Node.js Server!');
+http.createServer(function (request, response) {
+    var data = {'result': '<p>来自服务器端代码片段</p>'};
+    response.writeHead(200, {'Content-Type': 'application/json;charset=utf-8', 'Access-Control-Allow-Origin': '*'});
+    response.end(JSON.stringify(data));
 }).listen(8001);
 
 console.log('node.js server run at http://localhost:8001/...');
